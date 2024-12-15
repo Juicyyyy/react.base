46902dcd6628a0b9ade27e502ff482b7827e9c2d
Julia <y.diniullova@wb.ru>
2024-12-15 14:38:11 +0400

    Task-3

--- Task-3/inventory/src/App.tsx
+++ Task-3/inventory/src/App.tsx
@@ -1,30 +1,21 @@
-import { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Inventory from './components/Inventory';
 import { Item } from './types/Item';
 import inventoryDataJson from './data/inventory2.json'; // тут можно поменять файл инвентаря
 
-interface AppState {
-  inventoryData: Item[];
-}
+const App: React.FC = () => {
+  const [inventoryData, setInventoryData] = useState<Item[]>([]);
 
-class App extends Component<object, AppState> {
-
-  state = {
-    inventoryData: [],
-  };
-
-  componentDidMount() {
-    this.setState({ inventoryData: inventoryDataJson as Item[] });
-  }
-
-  render() {
-    return (
-      <>
-        <h1>Inventory</h1>
-        <Inventory data={this.state.inventoryData} />
-      </>
-    );
-  }
-}
+  useEffect(() => {
+    setInventoryData(inventoryDataJson as Item[]);
+  }, []);
+  
+  return (
+    <>
+      <h1>Inventory</h1>
+      <Inventory data={inventoryData} />
+    </>
+  );
+};
 
 export default App;
